a3054d6743674cdb82af259facd8a67d820b9b00
Omri Dadon <xxdadon25@gmail.com>
2026-06-28 15:19:55 +0300

    fix: add Sonner Toaster to App.jsx so toast() calls are displayed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+import { Toaster as SonnerToaster } from "sonner";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { queryClientInstance } from "@/lib/query-client";
 import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
@@ -113,6 +114,7 @@ function App() {
           </Routes>
         </Router>
         <Toaster />
+        <SonnerToaster position="top-center" richColors />
         <InstallPrompt />
       </QueryClientProvider>
     </AuthProvider>
