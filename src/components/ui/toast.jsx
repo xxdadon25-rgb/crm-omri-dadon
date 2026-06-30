f10ec89fc8d7b64db475fb918cd01bd9bb86cb90
Omri Dadon <xxdadon25@gmail.com>
2026-06-30 11:55:18 +0300

    fix: hide toast immediately on dismiss and reduce auto-remove delay to 5s

--- src/components/ui/toast.jsx
+++ src/components/ui/toast.jsx
@@ -37,7 +37,8 @@ const toastVariants = cva(
   }
 );
 
-const Toast = React.forwardRef(({ className, variant, ...props }, ref) => {
+const Toast = React.forwardRef(({ className, variant, open = true, ...props }, ref) => {
+  if (!open) return null;
   return (
     <div
       ref={ref}
